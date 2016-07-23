848c8885c1981c9f15ab75f6f3bcf6524d27bea8
Obasan Olajide <jideobs@gmail.com>
2016-07-23 15:13:37 +0100

    physician route registration, login, dashboard and get-auth-token implementation and test.

--- que/apps/users/__init__.py
+++ que/apps/users/__init__.py
@@ -1,3 +1,46 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template, redirect, url_for, jsonify
+from flask_login import login_user, login_required, current_user
+from passlib.apps import custom_app_context as pwd_context
+
+import forms
+from que import db
+from que.apps.users.models import physicians
 
 users_app = Blueprint('users', __name__)
+
+
+@users_app.route('/register', methods=['GET', 'POST'])
+def register_physician():
+    registration_form = forms.UserRegistrationForm(csrf_enabled=False)
+    if request.method == 'POST' and registration_form.validate():
+        physician_data = registration_form.data
+        physician_data['password'] = pwd_context.encrypt(registration_form.password.data)
+        physicians_model = physicians.PhysicianCollection(db.mongo.Physicians)
+        physician_data['_id'] = physicians_model.insert(physician_data)
+
+        login_user(physicians.PhysicianModel(physician_data))
+        return redirect(url_for('.dashboard'))
+    return render_template('register.html', form=registration_form)
+
+
+@users_app.route('/login', methods=['GET', 'POST'])
+def login_physician():
+    login_form = forms.UserLoginForm(csrf_enabled=False)
+    if request.method == 'POST' and login_form.validate():
+        login_user(physicians.PhysicianModel(login_form.physician))
+
+        return redirect(url_for('.dashboard'))
+    return render_template('login.html', form=login_form)
+
+
+@users_app.route('/get-auth-token', methods=['GET'])
+@login_required
+def get_auth_token():
+    token = current_user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+
+@users_app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html')
---
+++ tests/users.py
@@ -0,0 +1,55 @@
+import unittest
+
+import simplejson as json
+
+from que.main import flask_app
+from que import db
+
+
+class AppRoutesTestCases(unittest.TestCase):
+    def setUp(self):
+        self.app = flask_app.test_client()
+
+    def tearDown(self):
+        with flask_app.app_context():
+            db.client.drop_database('que_test')
+
+    def test_register_get(self):
+        rv = self.app.get('/register')
+        self.assertIn('Register', rv.data)
+
+    def register_physician(self):
+        physician_data = {'fullname': 'Olajide Obasan', 'email': 'testmail@domain.com', 'password': 'newlife',
+                          'username': 'username', 'confirm_password': 'newlife'}
+        return self.app.post('/register', data=physician_data, follow_redirects=True)
+
+    def test_register_physician(self):
+        rv = self.register_physician()
+        self.assertIn('Dashboard', rv.data)
+
+    def login_physician(self):
+        self.register_physician()
+        login_data = {'unique_id': 'username', 'password': 'newlife'}
+        return self.app.post('/login', data=login_data, follow_redirects=True)
+
+    def test_login_get(self):
+        rv = self.app.get('/login')
+        self.assertIn('Login', rv.data)
+
+    def test_login_physician(self):
+        rv = self.login_physician()
+        self.assertIn('Dashboard', rv.data)
+
+    def test_fail_login_physician(self):
+        rv = self.app.post('/login', follow_redirects=True)
+        self.assertIn('Login', rv.data)
+
+    def get_auth_token(self):
+        self.login_physician()
+        return self.app.get('/get-auth-token')
+
+    def test_get_auth_token(self):
+        rv = self.get_auth_token()
+        self.assertEqual(rv.status_code, 200)
+        resp_data = json.loads(rv.data)
+        self.assertIsNotNone(resp_data['token'])
