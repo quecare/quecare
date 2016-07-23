a48d16900fee1e17743c1c5b6fa080ba66c539a2
Obasan Olajide <jideobs@gmail.com>
2016-07-23 15:03:09 +0100

    PhysicianModel class for current_user object flask_login

--- que/apps/users/models/physicians.py
+++ que/apps/users/models/physicians.py
@@ -1,5 +1,10 @@
-from mongokat import Collection, Document
 from datetime import datetime
+
+from flask_login import UserMixin
+from mongokat import Collection, Document
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+
+from que import flask_app, db
 
 
 class Physician(Document):
@@ -9,3 +14,33 @@ class Physician(Document):
 
 class PhysicianCollection(Collection):
     document_class = Physician
+
+
+class PhysicianModel(UserMixin):
+    def __init__(self, physician_data):
+        for (field, value) in physician_data.items():
+            if field == '_id':
+                setattr(self, 'id', str(value))
+            else:
+                setattr(self, field, value)
+
+    def generate_auth_token(self):
+        s = Serializer(flask_app.config['SECRET_KEY'])
+        return s.dumps({'id': str(self.id)})
+
+    @classmethod
+    def get_physician(cls, physician_id):
+        physician = PhysicianCollection(db.mongo.Physicians).find_by_id(physician_id)
+        return PhysicianModel(physician)
+
+    @classmethod
+    def verify_auth_token(cls, token):
+        s = Serializer(flask_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except SignatureExpired:
+            return False
+        except BadSignature:
+            return False
+        physician_data = PhysicianCollection(db.mongo.Physicians).find_by_id(data['id'])
+        return PhysicianModel(physician_data)
