143247ad80419be1d2aadb76789258771fda906a
Obasan Olajide <jideobs@gmail.com>
2016-07-23 04:09:49 +0100

    flask app config.

--- que/__init__.py
+++ que/__init__.py
@@ -1,4 +1,16 @@
+import os
+
 from flask import Flask
+
+import config
 
 
 flask_app = Flask(__name__)
+
+env = int(os.environ.get('ENV'))
+if env == 1:
+    flask_app.config.from_object(config.ProductionConfig)
+elif env == 2:
+    flask_app.config.from_object(config.TestingConfig)
+else:
+    flask_app.config.from_object(config.DevelopmentConfig)
