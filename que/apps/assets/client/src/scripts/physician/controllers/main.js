d67d0966137d431a5458686f46fc78403cb2a7f5
Obasan Olajide <jideobs@gmail.com>
2016-07-28 20:29:42 +0100

    removed pageTitle func.

--- que/apps/assets/client/src/scripts/physician/controllers/main.js
+++ que/apps/assets/client/src/scripts/physician/controllers/main.js
@@ -1,19 +1,2 @@
-quePhysicianApp.controller('MainCtrl', function ($scope, $rootScope, $mdSidenav) {
-    var defaultTitle = 'Dr .O. Obasan';
-    var dataPageLinks = [{'title': 'questions', 'stateName': 'data.questions'},
-                {'title': 'video consults', 'stateName': 'data.videoConsults'}];
-
-    $rootScope.$on('$stateChangeSuccess', function (event, toState) {
-        $scope.pageTitle = (toState.data && toState.data.title) || defaultTitle;
-        var pageName = toState.name.split('.');
-        if (pageName[0] == 'data') {
-            $scope.pages = dataPageLinks;
-        } else {
-            $scope.pages = undefined;
-        }
-    });
-
-    $scope.toggleSideNav = function () {
-        $mdSidenav('left').toggle();
-    };
+quePhysicianApp.controller('MainCtrl', function ($scope, $rootScope) {
 });
