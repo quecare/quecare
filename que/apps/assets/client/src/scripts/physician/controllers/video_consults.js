94dd6720beac068ff8c1a64a94ecbe60fbe31a39
Obasan Olajide <jideobs@gmail.com>
2016-07-28 20:30:28 +0100

    getConsults appointments from controller.

--- que/apps/assets/client/src/scripts/physician/controllers/video_consults.js
+++ que/apps/assets/client/src/scripts/physician/controllers/video_consults.js
@@ -1,3 +1,13 @@
-quePhysicianApp.controller('VideoConsultsCtrl', function ($scope, AllVideoConsults) {
-    $scope.videoConsults = AllVideoConsults;
+quePhysicianApp.controller('VideoConsultsCtrl', function ($scope, VideoConsults) {
+    $scope.getConsults = function () {
+        $scope.gettingVideoConsults = true;
+        VideoConsults.getList()
+        .then(function (response) {
+            $scope.videoConsults = response;
+        }, function (reason) {
+            $scope.videoConsultsGetErr = reason.data.message;
+        }).finally(function () {
+            $scope.gettingVideoConsults = false;
+        });
+    };
 });
