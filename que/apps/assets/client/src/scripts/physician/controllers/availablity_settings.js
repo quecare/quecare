68d6e12e0bddeaa92c2c521042928a5303d5b0b1
Obasan Olajide <jideobs@gmail.com>
2016-07-31 02:09:44 +0100

    refactored for better testing and user experience.

--- que/apps/assets/client/src/scripts/physician/controllers/availablity_settings.js
+++ que/apps/assets/client/src/scripts/physician/controllers/availablity_settings.js
@@ -1,81 +1,62 @@
 quePhysicianApp.controller('AvailabilitySettingCtrl',
 function ($scope, Hours, AvailabilitySettings) {
-    Hours.getList()
-    .then(function (response) {
-        $scope.hours = response;
-    });
+    $scope.getHours = function () {
+        if ($scope.hours) return;
 
-    AvailabilitySettings.getList().then(function (response) {
+        $scope.gettingHours = true;
+        Hours.getList()
+        .then(function (response) {
+            $scope.hours = response;
+        }, function (response) {
+            $scope.getHoursErr = response.data.message;
+        }).finally(function () {
+            $scope.gettingHours = false;
+        });
+    };
 
-        if (response.length < 7) {
-            var days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-            var alreadySetDays = _.map(response, function (setting) {
-                var day = setting.day;
-                if (day) {
-                    return day;
-                }
-            });
-
+    $scope.getAvailabilitySettings = function () {
+        $scope.gettingSettings = true;
+        AvailabilitySettings.getList().then(function (response) {
             $scope.availabilitySettings = response;
-            var missingDays = _.difference(days, alreadySetDays);
-            for (i=0; i<missingDays.length; i++) {
-                var setting = _.extend({day: missingDays[i]}, AvailabilitySettings.one());
-                $scope.availabilitySettings.push(setting);
-            }
-        }
-    });
+        }, function (response) {
+            $scope.getAvailabilityErr = response.data.message;
+        }).finally(function () {
+            $scope.gettingSettings = false;
+        });
+    };
 
-    $scope.saveSetting = function (setting) {
-        if (setting.id) {
-            return setting.put()
-                .then(function (response) {
-                    _.extend(setting, response);
-                });
+    function saveSetting (setting) {
+        $scope.savingSettings = true;
+
+        setting.put()
+        .then(function (response) {
+            _.extend(setting, response);
+        }, function (reason) {
+            $scope.saveSettingError = reason.data.message;
+        }).finally(function () {
+            $scope.savingSettings = false;
+        });
+    };
+    $scope.saveSetting = saveSetting;
+
+    $scope.toggleHour = function (setting, hourId) {
+        var idx = setting.hours.indexOf(hourId);
+        if (idx == -1) {
+            setting.hours.push(hourId);
         } else {
-            return AvailabilitySettings.post(setting)
-               .then(function (response) {
-                    _.extend(setting, response);
-               });
+            setting.hours.splice(idx, 1);
         }
+        saveSetting(setting);
     };
 
-    function DialogCtrl ($scope, $mdDialog, setting, hours, saveSetting) {
-        $scope.setting = setting;
-        $scope.setting.hours = $scope.setting.hours || [];
-        $scope.hours = hours;
-        $scope.hide = function () {
-            $mdDialog.hide();
-        };
-
-        $scope.cancel = function () {
-            $mdDialog.cancel();
-        };
-
-        $scope.toggle = function (hourId) {
-            var idx = $scope.setting.hours.indexOf(hourId);
-            if (idx == -1) {
-                $scope.setting.hours.push(hourId);
-            } else {
-                $scope.setting.hours.splice(idx, 1);
-            }
-        };
-
-        $scope.exists = function (hourId) {
-            return $scope.setting.hours.indexOf(hourId) > -1;
-        };
-
-        $scope.saveSetting = function () {
-            $scope.saving = true;
-            saveSetting($scope.setting)
-            .catch(function (reason) {
-                $scope.errMsg = reason.data.message;
-            })
-            .finally(function () {
-                $scope.saving = false;
-            });
-        };
-    }
-
-    $scope.editSettings = function (ev, setting) {
+    $scope.exists = function (setting, hourId) {
+        return setting.hours.indexOf(hourId) > -1;
     };
+
+    $scope.toggleRepeatWeekly = function (setting) {
+        setting.repeat_weekly = !setting.repeat_weekly;
+        saveSetting(setting);
+    };
+
+    $scope.getAvailabilitySettings();
 });
