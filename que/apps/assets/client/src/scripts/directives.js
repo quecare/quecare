c2acc4491cc262fa84d09bfbc4eebb9e4fae88d6
Obasan Olajide <jideobs@gmail.com>
2016-07-31 02:10:25 +0100

    savingToast, loading and onEvent directives.

--- que/apps/assets/client/src/scripts/directives.js
+++ que/apps/assets/client/src/scripts/directives.js
@@ -1,2 +1,25 @@
 var queDirectives = angular.module('QueDirectives', []);
-
+queDirectives
+.directive('savingToast', function () {
+    return {
+        restrict: 'E',
+        template: '<div class="saving-toast">Saving...</div>'
+    }
+})
+.directive('loading', function () {
+    return {
+        restrict: 'E',
+        template: '<i class="fa fa-spinner fa-spin fa-2x"></i>'
+    }
+})
+.directive('onEvent', function () {
+    return {
+        restrict: 'A',
+        scope: {onEvent: '@', callback: '&'},
+        link: function (scope, ele, attrs, ctrl) {
+            ele.on(scope.onEvent, function () {
+                scope.callback();
+            })
+        }
+    }
+})
