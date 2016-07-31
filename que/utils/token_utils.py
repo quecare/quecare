bcf1f70dd797b85fc73d9b9b55dc7f98b38718a5
Obasan Olajide <jideobs@gmail.com>
2016-08-01 00:31:57 +0100

    initial commit.

---
+++ que/utils/token_utils.py
@@ -0,0 +1,18 @@
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+from que import flask_app
+
+
+def generate_confirmation_token(unique_id):
+    serializer = Serializer(flask_app.config['SECRET_KEY'])
+    return serializer.dumps(unique_id, salt=flask_app.config['SECURITY_PASSWORD_SALT'])
+
+
+def verify_token(token):
+    serializer = Serializer(flask_app.config['SECRET_KEY'])
+    try:
+        email = serializer.loads(token, salt=flask_app.config['SECURITY_PASSWORD_SALT'])
+    except SignatureExpired:
+        return False
+    except BadSignature:
+        return False
+    return email
